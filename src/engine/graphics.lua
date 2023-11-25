1a9513eeb4231452a8dd24d3f6772d56dde1d317
bhaskar10h <112790780+bhaskar10h@users.noreply.github.com>
2023-11-25 11:30:29 +0530

    Update graphics.lua

--- src/engine/graphics.lua
+++ src/engine/graphics.lua
@@ -2,10 +2,18 @@
 
 local std = require "lib.std"
 
-function love.graphics.rectWithBounds(mode, bounds)
+function love.graphics.rectWithBounds(mode, bounds, blendMode)
+    if not mode or not bounds then
+        error("love.graphics.rectWithBounds: Missing required arguments.")
+    end
+
+    love.graphics.setBlendMode(blendMode or "alpha")
     love.graphics.rectangle(mode, bounds.x1, bounds.y1, bounds.x2 - bounds.x1, bounds.y2 - bounds.y1)
 end
 
 function love.graphics.setColorWithTable(table)
-    love.graphics.setColor(table.red, table.green, table.blue, table.alpha)
+    local defaultColor = { red = 1, green = 1, blue = 1, alpha = 1 }
+    local color = table or defaultColor
+
+    love.graphics.setColor(color.red, color.green, color.blue, color.alpha)
 end
